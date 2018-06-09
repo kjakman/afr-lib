f76a0cfba2563381c3103fe542a46707898c75f9
Kjetil Larsen <github@kjakman.com>
2018-06-09 13:02:09 +0000

    running in Bangalore

--- www/js/vp/vp.js.php
+++ www/js/vp/vp.js.php
@@ -1,5 +1,5 @@
 <?php 
-require_once('set_env.inc');
+require_once('afr-lib/include/set_env.inc');
 header('Content-Type: application/x-javascript; charset=utf-8');
 $output = plugin_javascript($_GET);
 echo($output);  
